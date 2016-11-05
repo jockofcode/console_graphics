a9e1c3e8b3e1a6fc6f0a2b230c5d760b56532f3c
Branden Giacoletto <jockofcode@gmail.com>
2016-11-04 19:46:52 -0600

    rename number to index

--- lib/pallet.rb
+++ lib/pallet.rb
@@ -1,5 +1,10 @@
 class Pallet
   attr_accessor :pallet
+
+  def get_active_curses_pallet
+    @pallet.select{|k,v| v[:index] }.map{|k,v| [v[:index],v[:fg], v[:bg]] }.uniq
+  end
+  
   def self.load_from_file(filename)
     if !File.exists?(filename)
       File.open(filename,"w") {|f| f << {}.to_yaml }  
@@ -17,7 +22,7 @@ class Pallet
     @pallet = {}
   end
 
-  def add_color(name, fg, bg)
+  def add_color(name, fg, bg, options = {create_new_index: true})
     existing_slot = find_slot_by_name(name)
     if existing_slot
       existing_slot[:fg] = fg
@@ -25,25 +30,27 @@ class Pallet
       return name
     end
 
-    existing_number = find_slots_by_color_pair(fg, bg).map{|slot| slot[:number] }.uniq.first
+    existing_index = find_slots_by_color_pair(fg, bg).map{|slot| slot[:index] }.uniq.first
 
-    if existing_number
-      @pallet[name] = {number: existing_number, bg: bg, fg: fg}
+    if existing_index
+      @pallet[name] = {index: existing_index, bg: bg, fg: fg}
       return name
     end
 
-    next_number = ((1..255).to_a - @pallet.map{|k,v| v[:number]}).sort.first
-    if next_number == nil
+    next_index = ((1..255).to_a - @pallet.map{|k,v| v[:index]}).sort.first
+
+    if next_index == nil || options[:create_new_index] != true
       puts "No slots left for color"
+      @pallet[name] = {index: nil, bg: bg, fg: fg}
       return
     else
-      @pallet[name] = {number: next_number, bg: bg, fg: fg}
+      @pallet[name] = {index: next_index, bg: bg, fg: fg}
     end
   end
 
   def remove_color(color)
     if color.is_a?(Numeric)
-      color_slots = find_slots_by_number(color)
+      color_slots = find_slots_by_index(color)
     elsif color.is_a?(String)
       color_slots = [find_slot_by_name(color)].compact
     elsif color.is_a?(Array)
@@ -56,8 +63,8 @@ class Pallet
   def find_slot_by_name(name)
     @pallet[name]
   end
-  def find_slots_by_number(number)
-    @pallet.select{|n,v| v[:number] == number}
+  def find_slots_by_index(index)
+    @pallet.select{|n,v| v[:index] == index}
   end
   def find_slots_by_color_pair(fg, bg)
     @pallet.select{|n,v| v[:bg] == bg && v[:fg] == fg }
