bcfcc894245892257817af9d0f5caa66866aa96a
Branden Giacoletto <jockofcode@gmail.com>
2016-11-09 19:24:45 -0700

    rename keys_pressed var and use main_window
    Uses main_window for input. The console may not get reset when it
    closes, need to work that detail out

--- lib/main_loop.rb
+++ lib/main_loop.rb
@@ -6,68 +6,86 @@ class MainLoop
   end
 
   def run
-    key = nil
+    keys_pressed = []
     quit = false
     current_pallet = Pallet.load_from_file("pallet.yml")
     cur_line = Curses.lines / 2
     main_window
     loop do
-        main_window.setpos(cur_line, Curses.cols / 2)
-        main_window.addstr(key.to_s)
-        cur_line += 1
+      cur_line = Curses.lines / 2
+      main_window.setpos(cur_line, Curses.cols / 2)
+      main_window.addstr(keys_pressed.to_s) unless keys_pressed.empty?
+      cur_line += 1
 
       current_pallet.pallet.keys.each{ |key|
         color_name = key
         main_window.color_set( current_pallet.find_slot_by_name(key)[:index] )
         main_window.setpos(cur_line, Curses.cols / 2)
-        main_window.addstr("X")
+        main_window.addstr("X") unless keys_pressed.empty?
         main_window.color_set(0)
-        main_window.addstr(" " + color_name)
+        main_window.addstr(" " + color_name) unless keys_pressed.empty?
 
         cur_line += 1
       }
 
-        main_window.setpos(Curses.lines - 1, Curses.cols / 2)
-        main_window.addstr("Press Enter to Quit")
+      main_window.setpos(Curses.lines - 1, Curses.cols / 2)
+      main_window.addstr("Press Enter to Quit")
       main_window.refresh
-      key = [main_window.getch]
-      if key == [27]
-        sleep 0.01
-        key << main_window.getch
-        if key[1] == '['
-      end
-
-      quit = true if [13].include? key
+      keys_pressed = get_key
+      File.open("keys_pressed.yml","a"){|f| f << keys_pressed.to_yaml } if !keys_pressed.empty?
+      quit = true if [[13],[3],[26]].include? keys_pressed
       break if quit
-    end 
+    end
     main_window.close
+    Curses.noraw
+    Curses.crmode
+    Curses.echo
     @main_window = nil
   end
 
   def main_window
-    @main_window ||= (
-      setup_screen
-      setup_pallet
-      setup_keyboard
+    if !@main_window
+        @main_window = Curses::Window.new(*([0] * 4))
+        setup_screen
+        setup_pallet
+        setup_keyboard
+    end
+    @main_window
+  end
 
-      Curses::Window.new(*([0] * 4))
-    )
+  def get_key
+    keys = []
+    keys << main_window.getch
+    if keys.last == 27
+      keys << main_window.getch
+      if keys.last == "["
+        keys << main_window.getch
+        if keys.last == "M"
+          3.times{
+            keys << main_window.getch
+          }
+        end
+      end
+    end
+    keys.compact!
+    keys
   end
 
   def setup_screen
-      Curses.init_screen 
-      Curses.start_color 
+    Curses.init_screen 
+    Curses.start_color 
+    main_window.timeout= 0 #nodelay = 1
   end
 
   def setup_keyboard
-      Curses.noecho
-      Curses.nonl
-      Curses.stdscr.keypad(true)
-      Curses.raw
-        Curses.mousemask(Curses::BUTTON1_CLICKED|Curses::BUTTON2_CLICKED|Curses::BUTTON3_CLICKED|Curses::BUTTON4_CLICKED)
+    Curses.noecho
+    Curses.nonl
+    main_window.keypad(false)
+    Curses.raw
+    Curses.mousemask(Curses::BUTTON1_CLICKED|Curses::BUTTON2_CLICKED|Curses::BUTTON3_CLICKED|Curses::BUTTON4_CLICKED)
 
-     #Curses.crmode # mouse?
-      Curses.stdscr.nodelay = 1
+    #Curses.crmode # mouse?
+   #Curses.delay=0
   end
 
   def setup_pallet(pallet = Pallet.load_from_file("pallet.yml"))
