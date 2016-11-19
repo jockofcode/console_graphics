dac8565462f966d0b58c7d1017c1be9860c7620b
Branden Giacoletto <JockOfCode@gmail.com>
2016-11-19 10:32:51 -0700

    fix move_to_pos
    It appears that move_to_pos will ignore the request if the coordinates
    are outside the bounds of the current window.

--- bin/picker.rb
+++ bin/picker.rb
@@ -1,32 +1,32 @@
 #!/usr/bin/env ruby -Iapp -Ilib
-#require 'bundler/setup'
+require 'bundler/setup'
 require 'event_loop.rb'
 
 app = EventLoop.new
 start_time = Time.now
-print_char = "$"
 last_coords = [0,0]
-screen_size=[app.window_height, app.window_width].reverse
 
 
 app.run { |event|
   if event
     File.open("~event.yml","a"){|f| f << "count: #{app.events.count}\n" + event.to_yaml }
     last_coords = [event.data.cols,event.data.lines] if event.type == :mouse && event.method == :button_down
+screen_size=[(app.window_width/2), app.window_height]
     app.quit = true if [13,3,26].include?(event.data.char) # Ctrl<C>, Ctrl<Z>, Enter
     
 
-    #app.move_to_pos(*last_coords)
-    app.move_to_pos(*screen_size)
+    app.move_to_pos(*last_coords)
     app.select_color(1)
-    print_char = "#{app.window_height}x#{app.window_width}" 
+    print_char = "#" #{screen_size.to_yaml}\nvs\n#{last_coords.to_yaml}" 
     app.print_string(print_char)
     app.select_color(0)
 
   # Update status at the bottom, even if no event happened
   # app.move_to_pos(app.window_width - 1, app.window_height / 2)
   # ::FFI::NCurses.wmove(app.main_window, ::FFI::NCurses.getmaxy(app.main_window) - 1, ::FFI::NCurses.getmaxx(app.main_window) / 2)
-   # app.print_string("#{last_coords.first}x#{last_coords.last} Press Enter to Quit")
+    status = "#{last_coords.first}x#{last_coords.last} Press Enter to Quit"
+    app.move_to_pos((app.window_width - status.length) / 2, app.window_height)
+    app.print_string(status)
    #::FFI::NCurses.wrefresh(app.main_window)
    app.write_buffer
   end
